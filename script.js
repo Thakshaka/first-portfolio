0fdc75fe74f9cd0cf1bd13c93339270b63a63685
Thakshaka Rathnayake <76155498+Thakshaka@users.noreply.github.com>
2023-03-17 23:03:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// About Me Menu
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for(tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+
+    for(tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+// Navigation Menu For Smaller Screens
+
+var side_menu = document.getElementById("sidemenu");
+
+function openmenu() {
+    side_menu.style.right = "0";
+}
+
+function closemenu() {
+    side_menu.style.right = "-200px";
+}
+
+// Import Data To Google Form
+
+const scriptURL = '<SCRIPT URL>'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message sent successfully!"
+        setTimeout(function() {
+            msg.innerHTML = ""
+        }, 5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
